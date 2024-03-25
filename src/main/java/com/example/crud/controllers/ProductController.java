4e460a602cbf24ffe463ee6a1cdac1e8e3e3ecd9
Joao Victor <jv-amaralds@hotmail.com>
2024-03-25 07:49:31 -0300

    Primary resources

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,4 +1,28 @@
 package com.example.crud.controllers;
 
+import com.example.crud.domain.product.ProductRepository;
+import com.example.crud.domain.product.ProductDto;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping (value = "/products")
 public class ProductController {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @GetMapping
+    public ResponseEntity findAll(){
+        var allProducts = repository.findAll();
+       return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid ProductDto dto){
+        System.out.println(dto);
+        return ResponseEntity.ok().build();
+    }
 }
---
+++ src/main/java/com/example/crud/domain/product/ProductDto.java
@@ -0,0 +1,4 @@
+package com.example.crud.domain.product;
+
+public record ProductDto(String name, Number priceInCents) {
+}
